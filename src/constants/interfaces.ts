5426500b7e9b60c2b35c690474a646d99e3ad428
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-17 19:06:12 +0100

    Added way to store data related to projects/experience for use on the site, and typescript interfaces to help populate them accordingly.

---
+++ src/constants/interfaces.ts
@@ -0,0 +1,15 @@
+export interface PROJECT {
+    title: string,
+    description?: string,
+    link?: string,
+    company?: string,
+    tools: string[]
+}
+
+export interface EXPERIENCE {
+    company: string,
+    role: string[],
+    description: string,
+    dateStart: Date,
+    dateEnd: Date,
+}
