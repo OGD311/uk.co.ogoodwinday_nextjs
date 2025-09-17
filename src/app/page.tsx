7b81de8062bdf9865ac5e374a22edd0ca5e18f4b
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-17 17:44:21 +0100

    Added profile picture component and better styling to left side.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+import ProfilePicture from "@/components/ProfilePicture";
+import SectionsBar from "@/components/SectionsBar";
 import SocialLinks from "@/components/SocialLinks";
 import { useEffect, useRef, useState } from "react";
 
@@ -38,10 +40,24 @@ export default function Home() {
 
   return (
     <div className="flex h-screen">
-      <div className="w-1/4 bg-gray-400">
-        <SocialLinks />
+      <div className="w-1/4 bg-gray-400 h-full">
+        <div className="h-4/5 flex flex-col justify-between items-center mt-2">
+          <ProfilePicture />
+          <SectionsBar sections={["blog", "projects"]} activeSection={activeSection} />
+          <SocialLinks />
+        </div>
       </div>
-      <div className="w-3/4 h-full overflow-y-scroll" ref={scrollContainerRef}></div>
+      <div className="w-3/4 h-full overflow-y-scroll scroll-smooth" ref={scrollContainerRef}>
+
+        <div id="blog" className="section h-screen bg-amber-300">
+          BLOG
+        </div>
+        <div id="projects" className="section h-screen bg-blue-300">
+          PROJECTS
+        </div>
+
+
+      </div>
     </div>
   );
 }
