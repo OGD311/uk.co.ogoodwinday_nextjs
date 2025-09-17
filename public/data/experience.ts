2ac69e80265714215e39d1879398201f7968beb7
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-17 19:40:59 +0100

    Added some experience.

--- public/data/experience.ts
+++ public/data/experience.ts
@@ -1,3 +1,20 @@
-export const experience = {
+import { EXPERIENCE } from "@/constants/interfaces";
 
-}
+export const experience: EXPERIENCE[] = [
+    {
+        company: "Sheffield University Gaming Society (SLUGS)",
+        roles: ["Senior Technical Officer"],
+        description: "string",
+        dateStart: new Date(2025, 3, 1),
+    },
+    {
+        company: "Sheffield Ethical Student Hackers",
+        roles: ["Technical Officer"],
+        dateStart: new Date(2024, 10, 1),
+    },
+    {
+        company: "Enactus Sheffield",
+        roles: ["CodeCreators Lead", "CodeCreators Mentor"],
+        dateStart: new Date(2024, 10),
+    },
+]
