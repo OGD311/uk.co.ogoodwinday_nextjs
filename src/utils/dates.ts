d73701d0785b36edda9d79040c1cc8a1a66da23a
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-17 19:41:09 +0100

    Added case for if one month experience.

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,11 +1,18 @@
 export function formatExperienceDates(startDate: Date, endDate?: Date | null) {
+    // Case for if no end date (still working)
     if (!endDate) { 
         return `${startDate.toLocaleDateString("en-gb", { month: "long", year: "numeric"})} - Present`
     }
+    // Case for one month (or less)
     else if (startDate.getMonth() == endDate?.getMonth() && startDate.getFullYear() == endDate.getFullYear()) {
+        return `${startDate.toLocaleDateString("en-gb", { month: "long", year: "numeric" })}`
+    }
+    // Case for several months in one year
+    else if (startDate.getFullYear() == endDate.getFullYear()) {
         return `${startDate.toLocaleDateString("en-gb", { month: "long" })} - ${endDate.toLocaleDateString("en-gb", { month: "long", year: "numeric"})}`
     }
+    // Default case for 90% of experience
     else {
-        return `${startDate.getFullYear()} - ${endDate.getFullYear()}`
+        return `${startDate.toLocaleDateString("en-gb", { month: "long", year: "numeric"})} - ${endDate.toLocaleDateString("en-gb", { month: "long", year: "numeric"})}`
     }
 }
