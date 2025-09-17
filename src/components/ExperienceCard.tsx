42110093cdac9af99a966da67c1c242545137b72
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-17 19:41:26 +0100

    Added display of work dates.

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,11 +1,15 @@
 import { EXPERIENCE } from "@/constants/interfaces";
+import { formatExperienceDates } from "@/utils/dates";
 
 export default function ExperienceCard({ experience } : { experience: EXPERIENCE }) {
 
     return (
         <div className="flex w-4/5 border rounded-lg m-5 p-5 hover:-translate-y-3 transition duration-200">
-            <div className="flex w-full items-start justify-center">
-                <h1 className="text-lg mr-auto">{experience.company}</h1>
+            <div className="flex w-full items-start">
+                <div className="flex flex-col mr-auto">
+                    <h1 className="text-lg">{experience.company}</h1>
+                    <p>{formatExperienceDates(experience.dateStart, experience.dateEnd)}</p>
+                </div>
                 <ul className="flex flex-col items-end">
                     {experience.roles.map((role, i) => (
                         <li key={i} className={`${i === 0 ? "font-bold" : ""}`}>{role}</li>
