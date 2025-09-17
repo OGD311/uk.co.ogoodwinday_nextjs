a78948bf8060c131249a054876ecd2f209bac5df
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-17 22:19:52 +0100

    Added hover effects.

--- src/components/SectionsBar.tsx
+++ src/components/SectionsBar.tsx
@@ -7,7 +7,7 @@ export default function SectionsBar({ sections, activeSection } : { sections: st
         <div className="flex flex-col items-start">
             {sections.map((section, i) => (
                 <Link key={i} href={`#${section.toLowerCase()}`} className="w-full">
-                    <p className={`mt-2 p-3 text-2xl border-b-3 duration-150 transition ${activeSection == i ? 'bg-zinc-100' : 'border-transparent'}`}>{toTitlecase(section)}</p>
+                    <p className={`mt-2 p-3 text-2xl border-b-3 duration-150 transition hover:bg-zinc-200 ${activeSection == i ? 'bg-zinc-200' : 'border-transparent'}`}>{toTitlecase(section)}</p>
                 </Link>
             ))}
         </div>
