d1596bca0f9ad417412cf5cbe7f137de4891cccb
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-17 16:24:48 +0100

    Added scroll observer to allow me to track sections when scrolled over to make changes to UI.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,44 @@
+"use client"
+import { useEffect, useRef, useState } from "react";
+
 export default function Home() {
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+  const [activeSection, setActiveSection] = useState(0);
+
+  useEffect(() => {
+    if (!scrollContainerRef.current) return;
+
+    const sections = scrollContainerRef.current.querySelectorAll(".section");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const index = Array.from(sections).indexOf(entry.target as Element);
+            if (index !== -1) {
+              setActiveSection(index);
+            }
+          }
+        });
+      },
+      {
+        root: scrollContainerRef.current,
+        threshold: 0.5
+      }
+    );
+    
+    sections.forEach((section) => observer.observe(section));
+
+    return(() => {
+      sections.forEach((section) => observer.unobserve(section));
+    })
+  }, []);
+
+
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="flex h-screen">
+      <div className="w-1/4 bg-gray-400"></div>
+      <div className="w-3/4" ref={scrollContainerRef}></div>
     </div>
   );
 }
