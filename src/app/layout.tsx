cfe97901ec6a42a519833501b048fdd00439da93
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-19 21:12:49 +0100

    Added references to alternative icons in metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,16 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "OGD",
   description: "Oliver Goodwin-Day's Portfolio",
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+    other: [
+      { rel: 'icon', url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png'},
+      { rel: 'icon', url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png'},
+      {rel: "manifest", url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png"},
+      {rel: "manifest", url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png"},
+    ],
+  },
 };
 
 export default function RootLayout({
