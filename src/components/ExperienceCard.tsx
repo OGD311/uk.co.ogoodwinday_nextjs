057d4a3bd0ddafda8a0ad7f413d08830dc3e4151
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-19 15:10:58 +0100

    Added alternative card for if an experience has a related link.

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,9 +1,12 @@
 import { EXPERIENCE } from "@/constants/interfaces";
 import { formatExperienceDates } from "@/utils/dates";
+import Link from "next/link";
+import { CiShare1 } from "react-icons/ci";
+import { FaShare } from "react-icons/fa";
 
 export default function ExperienceCard({ experience } : { experience: EXPERIENCE }) {
 
-    return (
+    return experience.link == null ? (
         <div className="flex w-4/5 border rounded-lg m-5 p-5 hover:-translate-y-3 duration-200 transition">
             <div className="flex w-full items-start max-md:flex-col">
                 <div className="flex flex-col mr-auto">
@@ -17,5 +20,20 @@ export default function ExperienceCard({ experience } : { experience: EXPERIENCE
                 </ul>
             </div>
         </div>
-    );
+    ) : 
+    (
+        <Link href={experience.link} className="flex w-4/5 border rounded-lg m-5 p-5 hover:-translate-y-3 duration-200 transition">
+            <div className="flex w-full items-start max-md:flex-col">
+                <div className="flex flex-col mr-auto">
+                    <h1 className="text-lg max-md:text-xl flex items-center">{experience.company} <CiShare1 className="ml-5 text-xl max-md:text-4xl"/></h1>
+                    <p>{formatExperienceDates(experience.dateStart, experience.dateEnd)}</p>
+                </div>
+                <ul className="flex flex-col min-md:items-end max-md:mt-2">
+                    {experience.roles.map((role, i) => (
+                        <li key={i} className={`${i === 0 ? "font-bold" : ""}`}>{role}</li>
+                    ))}
+                </ul>
+            </div>
+        </Link>
+    )
 }
