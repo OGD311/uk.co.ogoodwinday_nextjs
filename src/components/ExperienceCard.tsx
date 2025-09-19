04558c2641c4f1e5a7803a97c590398371cef400
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-19 15:16:02 +0100

    Fixed mis-sizing of share button.

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -25,7 +25,10 @@ export default function ExperienceCard({ experience } : { experience: EXPERIENCE
         <Link href={experience.link} className="flex w-4/5 border rounded-lg m-5 p-5 hover:-translate-y-3 duration-200 transition">
             <div className="flex w-full items-start max-md:flex-col">
                 <div className="flex flex-col mr-auto">
-                    <h1 className="text-lg max-md:text-xl flex items-center">{experience.company} <CiShare1 className="ml-5 text-xl max-md:text-4xl"/></h1>
+                    <h1 className="text-lg max-md:text-xl flex items-center">
+                        {experience.company} 
+                        <CiShare1 className="ml-2 text-xl max-md:text-2xl flex-shrink-0" />
+                    </h1>
                     <p>{formatExperienceDates(experience.dateStart, experience.dateEnd)}</p>
                 </div>
                 <ul className="flex flex-col min-md:items-end max-md:mt-2">
