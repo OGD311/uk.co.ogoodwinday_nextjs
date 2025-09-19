8f243f4eeddb74c4b8e76467ae5d1abf7e6a4814
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-09-19 15:45:50 +0100

    Added some more fields to experience. Started to populate projects.

--- public/data/experience.ts
+++ public/data/experience.ts
@@ -6,15 +6,18 @@ export const experiences: EXPERIENCE[] = [
         roles: ["Senior Technical Officer"],
         description: "string",
         dateStart: new Date(2025, 3, 1),
+        link: "https://slugsoc.co.uk"
     },
     {
         company: "Sheffield Ethical Student Hackers",
         roles: ["Technical Officer"],
         dateStart: new Date(2024, 9, 21),
+        link: "https://shefesh.com"
     },
     {
         company: "Enactus Sheffield",
         roles: ["CodeCreators Lead", "CodeCreators Mentor"],
         dateStart: new Date(2024, 9, 16),
+        link: "https://www.enactussheffield.org/codecreators"
     },
 ];
--- public/data/projects.ts
+++ public/data/projects.ts
@@ -1,5 +1,17 @@
 import { PROJECT } from "@/constants/interfaces";
 
 export const projects: PROJECT[] = [
+    {
+        title: "RateMySociety",
+        tools: ["NextJS", "React", "Tailwind", "Prisma", "Cloudflare"],
+        description: "A review site, allowing University students to give honest, anonymous reviews of societies at their University. It makes heavy use of NextJS' server functions, as well as utilising Prisma as an ORM, and Cloudflare Turnstile to prevent abuse.",
+    },
+    {
+        title: "slugsoc.co.uk",
+        tools: ["AstroJS", "React", "Tailwind", "Cloudflare"],
+        description: "A rewriting of the current societies Google Page in Astro, allowing us to have greater control over the styling and layout of the site. Astro was used for the majority of the site, with React being used for the Events page as it pulled events from a Google App Script linked to our society calendar. The contact page is a reverse-engineered google form, allowing us to keep any submissions on our google drive, whilst allowing the frontend styling to match our site.",
+        link: "https://slugsoc.co.uk",
+        company: "Sheffield University Gaming Society (SLUGS)"
+    }
 
 ];
