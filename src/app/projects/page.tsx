52fe3c7302427faf398b1f4dd1eb1e5fce32f906
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-10-20 17:11:59 +0100

    Fixed issues with width of entries, added styling to buttons and fixed dark mode. Added back to main page link.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,42 +1,64 @@
+"use client"
 import Link from "next/link";
 import { projects } from "../../../public/data/projects";
 import { CiShare1 } from "react-icons/ci";
+import { FaArrowLeft } from "react-icons/fa";
+import DarkModeToggle from "@/components/DarkModeToggle";
 
 export default function Projects(){
 
 
     return projects ? (
-        <div className="flex w-full justify-center">
+        <div className="flex min-h-screen flex-col text-text dark:text-text-dark bg-bg dark:bg-bg-dark *:duration-200 *:transition">
+            <DarkModeToggle />
+            <div >
+                <Link href={"/"} className="flex align-middle items-center">
+                    <FaArrowLeft />
+                    <span className="ml-2">Back to the site</span>
+                </Link>
+            </div>
             <table className="m-2">
                 <thead>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Tools</th>
-                    <th>Links</th>
-                    <th>Company</th>
+                    <tr>
+                        <th className="p-2">Title</th>                            
+                        <th>Description</th>
+                        <th>Tools</th>
+                        <th>Links</th>
+                        <th>Company</th>
+                    </tr>
                 </thead>
                 <tbody>
                     {projects.map((project) => (
-                        <tr key={project.title} className="odd:bg-gray-50">
-                            <td>{project.title}</td>
-                            <td>{project.description}</td>
-                            <td>
+                        <tr key={project.title} className="odd:bg-gray-50 odd:dark:bg-gray-950 *:px-4 *:py-2">
+                            <td >{project.title}</td>
+                            <td className="w-3/5">{project.description}</td>
+                            <td className="flex flex-col items-center *:my-0.5">
                                 {project.tools.map(tool => 
                                     <p key={tool} className="rounded-2xl border w-fit p-1 bg-pill-body text-pill-text dark:bg-pill-body-dark dark:text-pill-text-dark">{tool}</p>
                                 )}
                             </td>
-                            {project.link && 
-                                <td>
-                                    <Link href={project.link}>
-                                        <CiShare1 />
-                                    </Link>
-                                </td>
+                            {project.link ? (
+                                    <td>
+                                        <Link href={project.link}>
+                                            <CiShare1 className="w-10 h-10 hover:bg-hover dark:hover:bg-hover-dark rounded-lg duration-200 transition p-2" />
+                                        </Link>
+                                    </td>
+                                ) 
+                                :
+                                (
+                                    <td></td>
+                                )
                             }
-                            {!project.link && <td>No Link</td>}
-                            {project.company && 
-                                <td>{project.company}</td>
+                            {project.company ?
+                                (
+                                    <td className="w-1/8 text-center">{project.company}</td>
+                                )
+                                :
+                                (
+                                    <td></td>
+                                )
                             }
-                            {!project.company && <p>For fun!</p>}
+
                         </tr>
                     ))}
                 </tbody>
